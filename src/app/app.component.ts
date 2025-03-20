7c56372b68dccd59d0b02a5ab9874a323cc349da
c225 <calvin.zegbeu@gmail.com>
2025-03-20 23:41:04 +0100

    Modifications jusqu'à la page 70 du pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { AssignmentsComponent } from './assignments/assignments.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule, AssignmentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
